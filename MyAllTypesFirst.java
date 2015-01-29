4c9973f0f84125d42557ed237f9816ea3b4cf5df
Cassini23 <nbalach1@binghamton.edu>
2015-01-28 19:42:11 -0500

    MyAllTypesFirst

---
+++ MyAllTypesFirst.java
@@ -0,0 +1,79 @@
+package reflection.util;
+
+public class MyAllTypesFirst {
+
+	//data members
+	int myInt = 0;
+	String myString = null;
+	double myDouble = 0.0;
+	long myLong = 0;
+	char myChar = '\0';
+	/**
+	 * @return the myInt
+	 */
+	public int getMyInt() {
+		return myInt;
+	}
+	/**
+	 * @param myInt the myInt to set
+	 */
+	public void setMyInt(int myIntIn) {
+	//	System.out.println("Method is being called for MyInt....."+myIntIn);
+		myInt = myIntIn;
+	}
+	/**
+	 * @return the myString
+	 */
+	public String getMyString() {
+		return myString;
+	}
+	/**
+	 * @param myString the myString to set
+	 */
+	public void setMyString(String myString) {
+	//	System.out.println("Method is being called for MyString....."+myString);
+		this.myString = myString;
+	}
+	/**
+	 * @return the myDouble
+	 */
+	public double getMyDouble() {
+		return myDouble;
+	}
+	/**
+	 * @param myDouble the myDouble to set
+	 */
+	public void setMyDouble(double myDouble) {
+	//	System.out.println("Method is being called for MyDouble....."+myDouble);
+		this.myDouble = myDouble;
+	}
+	/**
+	 * @return the myLong
+	 */
+	public long getMyLong() {
+		return myLong;
+	}
+	/**
+	 * @param myLong the myLong to set
+	 */
+	public void setMyLong(long myLong) {
+	//	System.out.println("Method is being called for MyLong....."+myLong);
+		this.myLong = myLong;
+	}
+	/**
+	 * @return the myChar
+	 */
+	public char getMyChar() {
+		return myChar;
+	}
+	/**
+	 * @param myChar the myChar to set
+	 */
+	public void setMyChar(char myChar) {
+	//	System.out.println("Method is being called for MyChar....."+myChar);
+		this.myChar = myChar;
+	}
+	
+	
+	
+}
