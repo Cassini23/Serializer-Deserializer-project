684e053eadad517d71d6dbe56147cd459184a269
Cassini23 <nbalach1@binghamton.edu>
2015-01-28 19:43:35 -0500

    MyAllTypesSecond

---
+++ MyAllTypesSecond.java
@@ -0,0 +1,76 @@
+package reflection.util;
+
+public class MyAllTypesSecond {
+	
+	//data members
+	int myIntS;
+	String myStringS;
+	float myFloatS;
+	short myShortS;
+	char myCharS;
+	/**
+	 * @return the myIntS
+	 */
+	public int getMyIntS() {
+		return myIntS;
+	}
+	/**
+	 * @param myIntS the myIntS to set
+	 */
+	public void setMyIntS(int myIntS) {
+	//	System.out.println("Method is being called for myInstS....."+myIntS);
+		this.myIntS = myIntS;
+	}
+	/**
+	 * @return the myStringS
+	 */
+	public String getMyStringS() {
+		return myStringS;
+	}
+	/**
+	 * @param myStringS the myStringS to set
+	 */
+	public void setMyStringS(String myStringS) {
+	//	System.out.println("Method is being called for myStringS....."+myStringS);
+		this.myStringS = myStringS;
+	}
+	/**
+	 * @return the myFloatS
+	 */
+	public float getMyFloatS() {
+		return myFloatS;
+	}
+	/**
+	 * @param myFloatS the myFloatS to set
+	 */
+	public void setMyFloatS(float myFloatS) {
+	//	System.out.println("Method is being called for myFloatS....."+myFloatS);
+		this.myFloatS = myFloatS;
+	}
+	/**
+	 * @return the myShortS
+	 */
+	public short getMyShortS() {
+		return myShortS;
+	}
+	/**
+	 * @param myShortS the myShortS to set
+	 */
+	public void setMyShortS(short myShortS) {
+	//	System.out.println("Method is being called for myShortS....."+myShortS);
+		this.myShortS = myShortS;
+	}
+	/**
+	 * @return the myCharS
+	 */
+	public char getMyCharS() {
+		return myCharS;
+	}
+	/**
+	 * @param myCharS the myCharS to set
+	 */
+	public void setMyCharS(char myCharS) {
+	//	System.out.println("Method is being called for myCharS....."+myCharS);
+		this.myCharS = myCharS;
+	}
+}
